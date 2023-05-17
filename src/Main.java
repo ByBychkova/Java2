39b3baf8a3a09037a7b29ff25506c945ad46eb8d
Bychkova Mariya <bychk-mariya@yandex.ru>
2023-05-17 12:53:55 +0300

    calkulation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int ticket = 13_676; //стоимость билета
+        int bonus = ticket / 20; //расчет бонусов
+
+        System.out.println("Стоимость билетра: " + (ticket));
+
+        System.out.println("Начисление бонусов:");
+        System.out.println(bonus);
     }
 }
